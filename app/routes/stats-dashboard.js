f4a5c595496bc71f276e1bdfd00753db5975a3c4
kcgolden <kcgolden@gmail.com>
2017-12-01 14:21:07 -0600

    updated to use new id based endpoint for stats

--- app/routes/stats-dashboard.js
+++ app/routes/stats-dashboard.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return this.store.findAll('stat');
+        return this.store.find('stat', 304);
     }
 });
